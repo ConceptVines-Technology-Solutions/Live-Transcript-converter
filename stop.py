a967084c27ee54ec1938061323de4c4bde3db8ab
Dinesh-wrk <160482742+Dinesh-wrk@users.noreply.github.com>
2024-07-11 17:42:27 +0530

    updates the service

--- stop.py
+++ stop.py
@@ -1,24 +1,22 @@
 import speech_recognition as sr
 import requests
 import json
-import time
 from flask import Flask, jsonify
 from concurrent.futures import ThreadPoolExecutor
 import uuid
 from flask_cors import CORS
+import threading
 
 app = Flask(__name__)
-CORS(app,origins="")
-executor = ThreadPoolExecutor(max_workers=2)
+CORS(app, origins="*")
+executor = ThreadPoolExecutor(max_workers=1)
 
 tasks = {}
 
-def convert_live_audio_to_text(task_id, text_file_path, endpoint_url,controller):
+def convert_live_audio_to_text(task_id, text_file_path, endpoint_url, stop_event):
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
     sentiment = "Casual"
-    if controller == False :
-        return
     print("Please start speaking...")
 
     with open(text_file_path, 'w') as file:
@@ -27,10 +25,9 @@ def convert_live_audio_to_text(task_id, text_file_path, endpoint_url,controller)
                 recognizer.adjust_for_ambient_noise(source)
                 print("Listening... (Press Ctrl+C to stop)")
 
-                while controller:
-                    
+                while not stop_event.is_set():
                     print("Say something!")
-                    audio_data = recognizer.listen(source)
+                    audio_data = recognizer.listen(source, timeout=5)
 
                     try:
                         text = recognizer.recognize_google(audio_data)
@@ -63,8 +60,6 @@ def convert_live_audio_to_text(task_id, text_file_path, endpoint_url,controller)
                             tasks[task_id]['status'] = 'failed'
                             tasks[task_id]['error'] = response.text
 
-                        
-
                     except sr.UnknownValueError:
                         print("Google Speech Recognition could not understand the audio")
                     except sr.RequestError as e:
@@ -80,25 +75,32 @@ def convert_live_audio_to_text(task_id, text_file_path, endpoint_url,controller)
 @app.route('/start_transcription', methods=['GET'])
 def start_transcription():
     task_id = str(uuid.uuid4())
-    tasks[task_id] = {'status': 'in_progress', 'sentiment': None, 'error': None}
+    stop_event = threading.Event()
+    tasks[task_id] = {'status': 'in_progress', 'sentiment': None, 'error': None, 'stop_event': stop_event}
     endpoint_url = 'http://172.191.181.102:8002/predict'  # Update with your endpoint URL if different
-    executor.submit(convert_live_audio_to_text, task_id, 'live_transcription.txt', endpoint_url,True)
+    executor.submit(convert_live_audio_to_text, task_id, f'{task_id}.txt', endpoint_url, stop_event)
     return jsonify({'task_id': task_id})
 
 @app.route('/get_sentiment/<task_id>', methods=['GET'])
 def get_sentiment(task_id):
     task = tasks.get(task_id)
     if task:
-        return jsonify(task)
+        # Create a copy of the task dictionary excluding non-serializable objects
+        task_info = {key: value for key, value in task.items() if key != 'stop_event'}
+        return jsonify(task_info)
     else:
         return jsonify({'error': 'Task ID not found'}), 404
-    
+
 @app.route('/stop_transcription/<task_id>', methods=['GET'])
 def stop_transcription(task_id):
-    endpoint_url = 'http://172.191.181.102:8002/predict'
-    convert_live_audio_to_text(task_id, 'live_transcription.txt', endpoint_url,False)
-    return jsonify({'status': 'stopped', 'task_id': task_id})
-    
+    task = tasks.get(task_id)
+    if task and 'stop_event' in task:
+        stop_event = task['stop_event']
+        stop_event.set()
+        tasks[task_id]['status'] = 'interrupted'
+        return jsonify({'status': 'stopped', 'task_id': task_id})
+    else:
+        return jsonify({'error': 'Task ID not found'}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
